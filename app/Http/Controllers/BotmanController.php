663c3d7629b6e162f27ecde91d80a2b438ecbf32
llisel123 <llisellopezestrada@gmail.com>
2023-09-25 20:10:33 -0600

    Cambios nuevos

--- app/Http/Controllers/BotmanController.php
+++ app/Http/Controllers/BotmanController.php
@@ -1,67 +1,90 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Conversations\Conversation;
 use BotMan\BotMan\Messages\Incoming\Answer;
 use BotMan\BotMan\Messages\Outgoing\Question;
 use BotMan\BotMan\Messages\Outgoing\Actions\Button;
 
+
+
 class BotmanController extends Controller
 {
     public function enterRequest()
+    {
+        $botman = app('botman');
+
+        $botman->hears('Hola', function (BotMan $bot) {
+            $this->askName($bot);
+        });
+
+        $botman->hears('ayuda', function (BotMan $bot) {
+            $this->askViolence($bot);
+        });
+
+        $botman->listen();
+    }
+
+    public function askName(BotMan $bot)
+    {
+        $bot->ask('¡Hola, me da gusto que te comuniques con nosotros! ¿Cómo te llamas?', function(Answer $answer) use ($bot) {
+            $name = $answer->getText();
+            $this->say('Encantado de conocerte, '.$name.'. Si necesitas que te brindemos ayuda, solo dime "ayuda".');
+        });
+    }
+   public function askViolence(BotMan $bot)
 {
-    $botman = app('botman');
-    $botman->hears('{message}', function(BotMan $botman, $message) {
-        if ($message == 'hola') {
-            $this->askName($botman);
+    $bot->ask('Estoy aquí para ayudarte. ¿Qué tipo de violencia estás experimentando?', function (Answer $answer) use ($bot) {
+        $response = strtolower($answer->getText());
+        
+        if (strpos($response, 'física') !== false) {
+            $this->askPhysicalViolenceDetails($bot);
+        } elseif (strpos($response, 'emocional') !== false) {
+            $this->askEmotionalViolenceDetails($bot);
+        } elseif (strpos($response, 'verbal') !== false) {
+            $this->askVerbalViolenceDetails($bot);
+        } elseif (strpos($response, 'no estoy seguro') !== false) {
+            $this->say('Si en algún momento necesitas ayuda o tienes dudas, no dudes en contactarnos.');
+        } elseif (strpos($response, 'ninguna') !== false) {
+            $this->say('Me alegra saber que estás bien. Si tienes otras preguntas, no dudes en preguntar.');
         } else {
-            $violenceKeywords = $this->checkForViolence($message);
-            if (!empty($violenceKeywords)) {
-                $this->askViolence($botman, $violenceKeywords);
-            } else {
-                $botman->reply("¡Hola! ¿Le puedo ayudar en algo?");
-            }
-        }  
+            $this->say('Lo siento, no pude entender tu respuesta. Por favor, elige una de las opciones disponibles o proporciona más detalles.');
+            $this->askViolence($bot);
+        }
     });
-    $botman->listen();
 }
 
-    public function askName(BotMan $botman)
-    {
-        $botman->ask('¡Hola! ¿Cómo te llamas?', function(Answer $answer) use ($botman) {
-            $name = $answer->getText();
-            $this->say('Encantada de conocerte, '.$name);
-        });
-    }
 
-    public function askViolence(BotMan $botman)
-    {
-        $instituciones = [
-            'Centro Naranja de Valle de Bravo',
-            'Centro de ayuda a la mujer',
-            // Agrega más enlaces de instituciones aquí
-        ];
-    
-        $botman->ask('Tu caso es grave porque se reconocieron las palabras de violencia. ¿Necesitas ayuda o información sobre cómo enfrentar la violencia?', function (Answer $answer) use ($botman, $instituciones) {
-            $response = strtolower($answer->getText());
-            if (strpos($response, 'si') !== false) {
-                $randomIndex = array_rand($instituciones);
-                $this->say('Aquí puedes encontrar recursos y organizaciones que pueden ayudarte: ' . $instituciones[$randomIndex]);
-            } else {
-                $this->say('Si en algún momento necesitas ayuda, no dudes en pedirla. Estoy aquí para ti.');
-            }
-        });
-    }
+public function askPhysicalViolenceDetails(BotMan $bot)
+{
+    $bot->ask('Por favor, proporciona más detalles sobre la violencia física que estás experimentando. ¿Dónde ocurrió? ¿Cómo te sientes al respecto?', function (Answer $answer) use ($bot) {
+        $details = $answer->getText();
+        // Puedes manejar la respuesta del usuario aquí
+        $this->say('Gracias por compartir eso. Si necesitas asistencia inmediata, te recomendamos que te pongas en contacto con las autoridades.');
+    });
+}
+
+public function askEmotionalViolenceDetails(BotMan $bot)
+{
+    $bot->ask('Por favor, proporciona más detalles sobre la violencia emocional que estás experimentando. ¿Cómo te hace sentir? ¿Quién está involucrado?', function (Answer $answer) use ($bot) {
+        $details = $answer->getText();
+        // Puedes manejar la respuesta del usuario aquí
+        $this->say('Gracias por compartir eso. Si necesitas apoyo emocional, no dudes en comunicarte con nosotros.');
+    });
+}
+
+public function askVerbalViolenceDetails(BotMan $bot)
+{
+    $bot->ask('Por favor, proporciona más detalles sobre la violencia verbal que estás experimentando. ¿Qué tipo de palabras o acciones has enfrentado? ¿Cómo te afecta?', function (Answer $answer) use ($bot) {
+        $details = $answer->getText();
+        // Puedes manejar la respuesta del usuario aquí
+        $this->say('Gracias por compartir eso. Si necesitas asesoramiento, estamos aquí para ayudarte.');
+    });
+}
+
     
 
-    public function checkForViolence($message)
-    {
-        $keywords = ['golpes','golpeando','golpe','golpeada','golpear','golpeo','golpeaste','golpeara',
-        'amenaza','amenazas','amenazando','empujo','empujar', 'empujones',
-        'grito','gritos','gritar','gritando','abusos','violacion','discriminacion','acoso',
-        'manipulacion','patadas'];
-        $pattern = '/' . implode('|', $keywords) . '/i';
-        return preg_match($pattern, $message);
-    }
 }
